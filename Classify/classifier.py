173d99ddbfacae2596caabe215a56263f833fa21
Merryl Rolan DMello <rolan86@gmail.com>
2018-05-29 16:12:32 +0530

    Update classifier.py removed unnecessary information

--- Classify/classifier.py
+++ Classify/classifier.py
@@ -41,37 +41,21 @@ def train_classifier_up(data, test):
         testing data passed to it separately """
     model = RandomForestClassifier()
     model.fit(data[0], data[1])
-    predictions = model.predict(test[0])
+    predictions = model.predict(test)
     return predictions
 
 def fuzzy_match():
     """ 79% fuzzy match """ 
-    # print fuzz.ratio('Heikkinen Lain', 'Heikkinen Miss Laina')
-    print fuzz.token_set_ratio('Heikkinen Lain', 'Heikkinen, Miss. Laina')
+    return fuzz.token_set_ratio('Heikkinen Lain', 'Heikkinen, Miss. Laina')
 
 def fuzzy_matcher(query, data):
     return fuzz.token_set_ratio(query, data)
 
 def get_fuzz_name(df, query, tolerance=75):
+    """ Create an additional column fuzzer which contains
+        fuzzy distance between names with tolerance upto 100 """
     if 'fuzzer' in df.columns:
         df.drop('fuzzer', axis=1)
     df['fuzzer'] = [fuzzy_matcher(query, row.Name) for row in df.itertuples()]
     return df[(df['fuzzer']>=tolerance)]
-
-if __name__ == '__main__':
-    data = get_df('../data/train.csv')
-    train = get_clean_data(data)
-    tdata = pd.DataFrame(get_exact_match(data, 'Heikkinen, Miss. Laina'))
-    out = get_clean_data(tdata)
-    print train_classifier_up(train, out)
-    name_tile = pd.DataFrame(np.tile('Heikkinen, Miss. Laina', train[2].shape), columns=['Name'])
-    #train[2]['fuzzer'] = train[2].apply(lambda x: fuzzy_matcher(name_tile, x))
-    #train[2]['fuzzer'] = [fuzzy_matcher('Heikkinen, Miss. Laina', row) for row in train[2].iterrows()]
-    #train[2]['fuzzer'] = [fuzzy_matcher('Heikkinen, Miss. Laina', row.Name) for row in train[2].itertuples()]
-    #print train[2]
-    print get_fuzz_name(train[2], 'Heikkinen Miss. La')
-    print get_fuzz_name(train[2], 'Behr,Karl Howel')
-    
-    
-    
 
