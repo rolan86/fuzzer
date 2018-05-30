1577be3b1a85d0858e2d241cdd9d0ab32d4875c4
Merryl Rolan DMello <rolan86@gmail.com>
2018-05-30 11:53:51 +0530

    Update main.py
    Update main.py with survival endpoint details and classification

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+
+from Classify import classifier
 from flask import Flask, jsonify, request
 
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
@@ -7,10 +11,15 @@ app = Flask(__name__)
 def index():
     return jsonify({'Hello':'World'})
 
-@app.route('/survival', methods=['GET'])
+@app.route('/api/v1/survival', methods=['GET'])
 def survival():
     if 'name' in request.args:
+        csv_url = os.path.join(SITE_ROOT, 'data', 'train.csv')
         query = request.args['name']
+        training_data = classifier.get_df(csv_url)
+        training_data = classifier.get_clean_data(training_data)
+        result = classifier.get_survival(training_data, query)
+        return jsonify(result)
     else:
         return jsonify({'error': 'Please specify a name parameter'})
     return jsonify({'name': query})
